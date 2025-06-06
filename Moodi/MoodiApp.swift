8e28249a3306fc0423d60a685bb79d25a9b7edc2
Maria Amzil <mini.mar@hotmail.com>
2025-06-06 12:35:57 +0200

    Initial Commit

---
+++ Moodi/MoodiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodiApp.swift
+//  Moodi
+//
+//  Created by Maria Amzil on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoodiUITests/MoodiUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MoodiUITestsLaunchTests.swift
+//  MoodiUITests
+//
+//  Created by Maria Amzil on 6/6/25.
+//
+
+import XCTest
+
+final class MoodiUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
